0d30c6c172b1d9c2e2f80ebf934d7b6f65d0257a
AntoineAcquart <antoine.acquart@gmail.com>
2021-11-04 10:35:53 +0100

    init

--- frontend/src/app/services/shop.service.ts
+++ frontend/src/app/services/shop.service.ts
@@ -16,24 +16,4 @@ export class ShopService {
     public register(username: string, password: string): Observable<any> {
         return this.http.post(this.url + "register", { username, password });
     }
-
-    public login(username: string, password: string): Observable<any> {
-        return this.http.post(this.url + "login", { username, password });
-    }
-
-    public checkToken(): Observable<any> {
-        return this.http.get(this.url + "check-token", { headers: { authorization: localStorage.getItem('token') } });
-    }
-
-    isLogged() {
-        if (localStorage.getItem('token') && localStorage.getItem('username')) {
-            return true;
-        }
-        return false;
-    }
-
-    logout() {
-        localStorage.removeItem('token');
-        localStorage.removeItem('username');
-    }
 }
